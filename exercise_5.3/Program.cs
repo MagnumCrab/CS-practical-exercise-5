dc458389890b9c75ef61a51472758adde61e62fb
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-20 20:12:42 +0300

    add exercise_5.3

---
+++ exercise_5.3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 38: Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] Array = new double[10];
+  for (int i = 0; i < Array.Length; i++ )
+  {
+    Array[i] = new Random().Next(1, 10);
+    Console.Write(Array[i] + " ");
+  }
+
+double max = Array[0];
+double min = Array[0];
+
+  for (int i = 1; i < Array.Length; i++)
+  {
+    if (max < Array[i])
+    {
+      max = Array[i];
+    }
+        if (min > Array[i])
+    {
+      min = Array[i];
+    }
+  }
+  double decision = max - min;
+
+  Console.WriteLine($"\nРазница между между максимальным и минимальным элементами массива = {decision}");
